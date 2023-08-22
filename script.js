b1fa4557437ab5f6a2aba6837feb7fb04e4be000
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-08-22 18:34:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function threeSum(arr, target) {
 // write your code here
+	let result = nums[0] + nums[1] + nums[nums.length - 1];
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 2; i++) {
+        let start = i + 1, end = nums.length - 1;
+        while (start < end) {
+            let sum = nums[i] + nums[start] + nums[end];
+            if (sum > target) {
+                end--;
+            } else {
+                start++;
+            }
+            if (Math.abs(sum - target) < Math.abs(result - target)) {
+                result = sum;
+            }
+        }
+    }
+    return result;
   
-}
+};
 
 module.exports = threeSum;
